0feba11f8c67fafe33fe8f1088a904eb5b4b8bf8
mads2ez <maximsivtsev@gmail.com>
2020-04-26 00:04:21 +0300

    analytics service

---
+++ Ramp Builder/Model/AmplitudeAnalyticsService.swift
@@ -0,0 +1,9 @@
+//
+//  AmplitudeAnalyticsService.swift
+//  Ramp Builder
+//
+//  Created by Madsbook on 25.04.2020.
+//  Copyright © 2020 Maxim Sivtcev. All rights reserved.
+//
+
+import Foundation
---
+++ Ramp Builder/Model/AnalyticsService.swift
@@ -0,0 +1,14 @@
+//
+//  Analytics.swift
+//  Ramp Builder
+//
+//  Created by Madsbook on 25.04.2020.
+//  Copyright © 2020 Maxim Sivtcev. All rights reserved.
+//
+
+import Foundation
+
+protocol AnalyticsService {
+    func initialize()
+    func logEvent(_ :String)
+}
