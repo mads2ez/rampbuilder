f6c709abeef0c4a1f3b9ccc3ada30ac189fc3c09
mads2ez <maximsivtsev@gmail.com>
2020-04-29 20:47:11 +0300

    added infoview

---
+++ Ramp Builder/ViewModel/GapViewModel.swift
@@ -0,0 +1,42 @@
+//
+//  GapViewModel.swift
+//  Ramp Builder
+//
+//  Created by Madsbook on 11.02.2020.
+//  Copyright © 2020 Maxim Sivtcev. All rights reserved.
+//
+
+import SwiftUI
+
+class GapViewModel: ObservableObject {
+    @Published var gapParams: GapParams
+    
+    let store: GapParamsStore
+    
+    init(store: GapParamsStore) {
+        self.store = store
+        self.gapParams = store.get() ?? GapParams.defaultParams
+    }
+    
+    convenience init(params: GapParams) {
+        self.init(store: GapParamsUserDefaults())
+        self.gapParams = params
+    }
+}
+
+extension GapViewModel {
+    func refresh() {
+        self.gapParams = store.get() ?? GapParams.defaultParams
+    }
+    
+    var inputView: some View {
+        let inputView = InputView(viewModel: .init(store: self.store))
+        return inputView
+    }
+    
+    var infoView: some View {
+        let infoView = InfoView()
+        return infoView
+    }
+    
+}
